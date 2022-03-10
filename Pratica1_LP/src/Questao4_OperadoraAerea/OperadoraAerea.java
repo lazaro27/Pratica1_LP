6ac3da5659713aab412aa1f7dd3adeceea87af42
Lázaro Barbosa <lazarogbarbosa@gmail.com>
2022-03-10 16:24:26 -0300

    implementei switch case

---
+++ Pratica1_LP/src/Questao4_OperadoraAerea/OperadoraAerea.java
@@ -0,0 +1,15 @@
+package Questao4_OperadoraAerea;
+
+public class OperadoraAerea {
+
+	public static void main(String[] args) {
+	
+		System.out.println("OPERADORA AÉREA");
+		System.out.println("Digite uma das opções abaixo: ");
+		System.out.println("1-Cadastrar Passageiro;\n2-Check in; \n3-Cancelar Voo; \n4-Sair;");
+		
+		
+
+	}
+
+}
